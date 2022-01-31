ce638b9af2d1b215707be16dd7b1e4873ad14c5d
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-01-31 16:51:30 +0000

    empty notesList as default tested

--- notesModel.test.js
+++ notesModel.test.js
@@ -1,16 +1,19 @@
-const { expect } = require('@jest/globals');
-const NotesModel = require('./notesModel')
+const { expect } = require("@jest/globals");
+const NotesModel = require("./notesModel");
 
 describe("NotesModel class", () => {
   const model = new NotesModel();
+  it("returns empty notesList as defaultg", () => {
+    expect(model.notesList).toEqual([]);
+  });
   it("should add note", () => {
     model.add("Buy Milk");
     model.add("Go to the gym");
     expect(model.getNotes()).toEqual(["Buy Milk", "Go to the gym"]);
   });
 
-  it('returns an empty noteList', () => {
-      model.reset()
-      expect(model.getNotes()).toEqual([])
-  })
+  it("returns an empty noteList", () => {
+    model.reset();
+    expect(model.getNotes()).toEqual([]);
+  });
 });
